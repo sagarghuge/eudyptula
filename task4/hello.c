db5182450ceee4581632bb3cf6565d1cc027b22e
Sagar Ghuge <ghugesss@gmail.com>
2017-01-10 00:44:17 -0800

    added task1 file to task4

---
+++ task4/hello.c
@@ -0,0 +1,19 @@
+#include <linux/kernel.h>
+#include <linux/module.h>
+#include <linux/init.h>
+
+static int __init hello_init(void)
+{
+	printk(KERN_DEBUG "Hello World\n");
+	return 0;
+}
+
+static void __exit hello_exit(void)
+{
+}
+
+module_init(hello_init);
+module_exit(hello_exit);
+
+MODULE_AUTHOR("7950e1ebd46e");
+MODULE_LICENSE("GPL");
